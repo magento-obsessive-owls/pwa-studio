2972c639d750377e53a569be88a1e600a9e05417
Ievgen Kolesov <ikolesov@adobe.com>
2020-10-05 22:31:43 -0500

    PWA-815: Enable shoppers to change their currency using a currency switcher

--- packages/venia-ui/lib/components/CurrencySymbol/__tests__/currencySymbol.spec.js
+++ packages/venia-ui/lib/components/CurrencySymbol/__tests__/currencySymbol.spec.js
@@ -1,35 +1,56 @@
 import React from 'react';
-import IntlPolyfill from 'intl';
+import { IntlProvider } from 'react-intl';
+import waitForExpect from 'wait-for-expect';
 
 import { createTestInstance } from '@magento/peregrine';
 import CurrencySymbol from '../currencySymbol';
 
-if (!global.Intl.NumberFormat.prototype.formatToParts) {
-    // No `Intl`, so use and load the polyfill.
-    global.Intl = IntlPolyfill;
-    require('intl/locale-data/jsonp/en.js');
-}
+test('Renders a USD symbol', async () => {
+    const instance = createTestInstance(
+        <IntlProvider locale={'en-US'}>
+            <CurrencySymbol
+                currencyCode="USD"
+                currencyDisplay={'narrowSymbol'}
+            />
+        </IntlProvider>
+    );
 
-test('Renders a USD symbol', () => {
-    const instance = createTestInstance(<CurrencySymbol currencyCode="USD" />);
-
-    expect(instance.toJSON()).toMatchSnapshot();
+    await waitForExpect(() => {
+        expect(instance.toJSON()).toMatchSnapshot();
+    });
 });
 
-test('Renders a EUR symbol', () => {
-    const instance = createTestInstance(<CurrencySymbol currencyCode="EUR" />);
+test('Renders a EUR symbol', async () => {
+    const instance = createTestInstance(
+        <IntlProvider locale={'en-US'}>
+            <CurrencySymbol
+                currencyCode="EUR"
+                currencyDisplay={'narrowSymbol'}
+            />
+        </IntlProvider>
+    );
 
-    expect(instance.toJSON()).toMatchSnapshot();
+    await waitForExpect(() => {
+        expect(instance.toJSON()).toMatchSnapshot();
+    });
 });
 
-test('Allows custom classname', () => {
+test('Allows custom classname', async () => {
     const classes = {
         currency: 'curr'
     };
 
     const instance = createTestInstance(
-        <CurrencySymbol currencyCode="USD" classes={classes} />
+        <IntlProvider locale={'en-US'}>
+            <CurrencySymbol
+                currencyCode="USD"
+                classes={classes}
+                currencyDisplay={'narrowSymbol'}
+            />
+        </IntlProvider>
     );
 
-    expect(instance.toJSON()).toMatchSnapshot();
+    await waitForExpect(() => {
+        expect(instance.toJSON()).toMatchSnapshot();
+    });
 });
--- packages/venia-ui/lib/components/CurrencySymbol/currencySymbol.js
+++ packages/venia-ui/lib/components/CurrencySymbol/currencySymbol.js
@@ -1,37 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { FormattedNumberParts } from 'react-intl';
 import { string, shape } from 'prop-types';
-import { useIntl } from 'react-intl';
-import patches from '@magento/peregrine/lib/util/intlPatches';
+import { shouldPolyfill } from '@formatjs/intl-numberformat/should-polyfill';
+
+const polyfill = async () => {
+    await import('@formatjs/intl-numberformat/polyfill');
+    if (Intl.NumberFormat.polyfilled) {
+        await import('@formatjs/intl-numberformat/locale-data/en');
+    }
+};
 
 /**
  * The CurrencySymbol component is used to extract currency symbol from Intl.NumberFormat.
- *
- * Formatting of prices and currency symbol selection is handled entirely by the ECMAScript Internationalization API available in modern browsers.
- *
- * A [polyfill][] is required for any JavaScript runtime that does not have [Intl.NumberFormat.prototype.formatToParts][].
- *
- * [polyfill]: https://www.npmjs.com/package/intl
- * [Intl.NumberFormat.prototype.formatToParts]: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/DateTimeFormat/formatToParts
+ * https://formatjs.io/docs/react-intl/components/#formattednumberparts
  */
-
 const CurrencySymbol = props => {
-    const { locale } = useIntl();
-    const { currencyCode, classes, currencyDisplay } = props;
+    const { currencyCode, currencyDisplay, classes } = props;
+    const [pollyfilled, setPollyfilled] = useState(false);
 
-    // If the optional locale prop is not provided or is undefined,
-    // the runtime's default locale is used in the Intl.NumberFormat() constructor.
-    const parts = patches.toParts.call(
-        new Intl.NumberFormat(locale, {
-            style: 'currency',
-            currencyDisplay: currencyDisplay,
-            currency: currencyCode
-        }),
-        0
+    const partsMap = (part, i) => {
+        const key = `${i}-${part.value}`;
+        return (
+            part.type === 'currency' && (
+                <span key={key} className={classes.currency}>
+                    {part.value}
+                </span>
+            )
+        );
+    };
+
+    useEffect(() => {
+        if (shouldPolyfill()) {
+            polyfill().then(() => {
+                setPollyfilled(true);
+            });
+        }
+    }, [setPollyfilled]);
+
+    if (shouldPolyfill() && !pollyfilled) return null;
+
+    return (
+        <FormattedNumberParts
+            value={0}
+            currencyDisplay={currencyDisplay}
+            style="currency"
+            currency={currencyCode}
+        >
+            {parts => parts.map(partsMap)}
+        </FormattedNumberParts>
     );
-
-    const symbol = parts.find(part => part.type === 'currency');
-
-    return <span className={classes.currency}>{symbol.value}</span>;
 };
 
 CurrencySymbol.propTypes = {
